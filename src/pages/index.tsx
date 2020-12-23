8f99300d7376f9c07d33836696ef1ebaad521ae4
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2020-12-23 17:18:47 +0100

    Add initial content for landingpage.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,52 @@
-import React from "react";
-import Head from "next/head";
-import { Flex, Link, Heading, Text, useTheme } from "@chakra-ui/core";
-import { FaStrava, FaInstagram } from "react-icons/fa";
+import React from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import { FaStrava, FaInstagram } from 'react-icons/fa';
+
+import KomootIcon from '@src/components/icons/KomootIcon';
 
 const Home: React.FC<{}> = () => {
-  const theme = useTheme();
   return (
     <>
       <Head>
-        <title>Gruppettoruhr</title>
+        <title>Gruppettoruhr - Home</title>
       </Head>
-
-      <Flex
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        h="100vh"
-        w="100%"
-      >
-        <Heading as="h1" size="2xl">
-          Gruppettoruhr
-        </Heading>
-
-        <Flex p="20px">
-          <Link href="https://www.strava.com/clubs/gruppettoruhr" isExternal>
-            <FaStrava size={theme.fontSizes["4xl"]} />
-          </Link>
-          <Link href="https://www.instagram.com/gruppettoruhr/" isExternal>
-            <FaInstagram size={theme.fontSizes["4xl"]} />
-          </Link>
-        </Flex>
-
-        <Text>More content coming soon</Text>
-      </Flex>
+      <div className="container mx-auto">
+        <main className="h-screen flex items-center justify-center flex-col">
+          <div className="m-5 animate-fadeInFast">
+            <Image
+              src="/logos/full-black.svg"
+              alt="gruppettoruhr logo"
+              width="300"
+              height="250"
+              layout="intrinsic"
+            />
+          </div>
+          <span className="grid grid-cols-3 gap-6 animate-fadeInMedium">
+            <a
+              href="https://www.instagram.com/gruppettoruhr/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram className="h-7 w-7 transition duration-300 ease-in-out transform hover:scale-150" />
+            </a>
+            <a
+              href="https://www.strava.com/clubs/gruppettoruhr-690171"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaStrava className="h-7 w-7 transition duration-300 ease-in-out transform hover:scale-150" />
+            </a>
+            <a
+              href="https://www.komoot.de/user/1704741799149"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <KomootIcon className="h-7 w-7 transition duration-300 ease-in-out transform hover:scale-150" />
+            </a>
+          </span>
+        </main>
+      </div>
     </>
   );
 };
