e7709f165269ba1ba47556b049d19d88faab0803
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-12 00:54:39 +0100

    Move base container alignment and fade-in animation to _app component.

--- packages/website/src/pages/_app.tsx
+++ packages/website/src/pages/_app.tsx
@@ -5,7 +5,11 @@ import 'tailwindcss/tailwind.css';
 import '@src/styles/globals.css';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className="container mx-auto animate-fadeInFast">
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default App;
