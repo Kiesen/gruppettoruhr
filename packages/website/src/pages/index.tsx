298711b073d0ba1c04d136cf6a333aeecf1bc0bd
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-12 01:01:30 +0100

    Remove fade-in animation from index page.

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -14,40 +14,40 @@ const Home: React.FC<{}> = () => {
       </Head>
 
       <NavigationBar />
-          <div className="m-5 animate-fadeInFast">
-            <Image
-              src="/logos/full-black.svg"
-              alt="gruppettoruhr logo"
-              width="300"
-              height="250"
-              layout="intrinsic"
-            />
-          </div>
-          <span className="grid grid-cols-3 gap-6 animate-fadeInMedium">
-            <a
-              href="https://www.instagram.com/gruppettoruhr/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaInstagram className="h-7 w-7 transition duration-300 ease-in-out transform hover:scale-150" />
-            </a>
-            <a
-              href="https://www.strava.com/clubs/gruppettoruhr-690171"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaStrava className="h-7 w-7 transition duration-300 ease-in-out transform hover:scale-150" />
-            </a>
-            <a
-              href="https://www.komoot.de/user/1704741799149"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <KomootIcon className="h-7 w-7 transition duration-300 ease-in-out transform hover:scale-150" />
-            </a>
-          </span>
-        </main>
-      </div>
+      <main className="h-screen flex items-center justify-center flex-col">
+        <div className="m-5">
+          <Image
+            src="/logos/full-black.svg"
+            alt="gruppettoruhr logo"
+            width="300"
+            height="250"
+            layout="intrinsic"
+          />
+        </div>
+        <span className="grid grid-cols-3 gap-6">
+          <a
+            href="https://www.instagram.com/gruppettoruhr/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram className="h-7 w-7 transition duration-300 ease-in-out transform hover:scale-150" />
+          </a>
+          <a
+            href="https://www.strava.com/clubs/gruppettoruhr-690171"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaStrava className="h-7 w-7 transition duration-300 ease-in-out transform hover:scale-150" />
+          </a>
+          <a
+            href="https://www.komoot.de/user/1704741799149"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <KomootIcon className="h-7 w-7 transition duration-300 ease-in-out transform hover:scale-150" />
+          </a>
+        </span>
+      </main>
     </>
   );
 };
