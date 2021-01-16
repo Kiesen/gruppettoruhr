ce39f6a1ccb34a555a8021295d9adcf01e5befb9
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-16 01:47:02 +0100

    Add wip gifs page.

---
+++ packages/website/src/pages/gifs.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Gifs: FC<{}> = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="flex">ELEMENT</div>
+    </div>
+  );
+};
+
+export default Gifs;
