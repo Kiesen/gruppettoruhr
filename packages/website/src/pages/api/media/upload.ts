c85e84ee6e5e71198027bf667d5f273a31404bce
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-18 19:57:37 +0100

    Add consts for the image upload.

---
+++ packages/website/src/pages/api/media/upload.ts
@@ -0,0 +1,58 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import s3 from '@src/utils/s3';
+
+import {
+  FIELD_NAME,
+  MIME_TYPES,
+  MAX_FILES,
+  MAX_FILE_SIZE,
+} from '@src/consts/mediaUpload';
+
+import { BUCKET_MEDIA_PREFIX, S3_BUCKET_ID } from '@src/consts/s3';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const uploadMiddleware = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: S3_BUCKET_ID,
+    acl: 'public-read',
+    contentType(_, file, cb) {
+      cb(null, file.mimetype);
+    },
+    key(_, file, cb) {
+      cb(null, `${BUCKET_MEDIA_PREFIX}/${file.originalname}`);
+    },
+  }),
+  fileFilter: (_, file, cb) => {
+    if (MIME_TYPES.includes(file.mimetype)) {
+      cb(null, true);
+    }
+    cb(null, false);
+  },
+  limits: {
+    fileSize: MAX_FILE_SIZE,
+    files: MAX_FILES,
+  },
+}).array(FIELD_NAME);
+
+export default (
+  req: NextApiRequest,
+  res: NextApiResponse
+): void | Promise<void> => {
+  if (req.method === 'POST') {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    uploadMiddleware(req as any, res as any, (err) => {
+      if (err) console.error(err);
+    });
+    res.status(200).send({});
+  } else {
+    res.status(405).send({});
+  }
+};
