04d40f65f9c6debc6cf4bc8181ae5e50af69b6f1
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-22 00:34:58 +0100

    Add tailwindcss classes for the media page.

---
+++ packages/website/src/styles/media.ts
@@ -0,0 +1,19 @@
+export const dropzoneClasses = `
+  mb-12 
+  h-16
+  bg-white 
+  border-2
+  border-dotted
+  rounded-xl
+  flex flex-col 
+  justify-center 
+  cursor-pointer
+  items-center 
+  transition 
+  duration-300 
+  ease-in-out 
+  transform 
+  hover:scale-105 
+  hover:bg-gray-100
+  focus:outline-none
+`;
