07fa285114a4aa1e9a226ec2683c1b1523f8dabf
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-22 00:34:34 +0100

    Add tailwindcss classes for primary and secoundary buttons.

---
+++ packages/website/src/styles/buttons.ts
@@ -0,0 +1,34 @@
+export const primaryActionButtonClasses = ` 
+  w-full 
+  p-1 
+  rounded-md
+  flex 
+  items-center 
+  justify-center 
+  bg-black 
+  text-white
+  transition 
+  duration-300 
+  ease-in-out 
+  transform 
+  hover:scale-105
+  focus:outline-none
+`;
+
+export const secondaryActionButtonClasses = ` 
+  w-full 
+  p-1 
+  border-2
+  border-gray-300
+  bg-white
+  rounded-md
+  flex 
+  items-center 
+  justify-center 
+  transition 
+  duration-300 
+  ease-in-out 
+  transform 
+  hover:scale-105
+  focus:outline-none
+`;
