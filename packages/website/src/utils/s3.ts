6a8b5481fc9bccd2e3d1a82079e1350a1d70dd1a
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-21 01:56:43 +0100

    Update and add s3 helper functions.

--- packages/website/src/utils/s3.ts
+++ packages/website/src/utils/s3.ts
@@ -6,6 +6,7 @@ import {
   S3_BUCKET_ID,
   BUCKET_MEDIA_PREFIX,
 } from '@src/consts/s3';
+import { ByteBuffer } from 'aws-sdk/clients/cloudtrail';
 
 const spacesEndpoint = new aws.Endpoint(S3_ENDPOINT);
 
@@ -15,28 +16,65 @@ const s3 = new aws.S3({
   secretAccessKey: process.env.DO_S3_SPACES_SECRET,
 });
 
-const getStaticImageURLs = (): Promise<string[]> => {
-  return new Promise((res) => {
-    s3.listObjects(
+const getMediaURLS = async (): Promise<string[]> => {
+  return new Promise((resolve, reject) => {
+    s3.listObjectsV2(
       {
         Bucket: S3_BUCKET_ID,
         Prefix: BUCKET_MEDIA_PREFIX,
       },
       (err, data) => {
         if (err) {
-          return res([]);
+          return reject('Error while fetching data from S3 service.');
         } else {
           const imageURLs = [];
           data.Contents.forEach((content) => {
             if (content.Size > 0)
               return imageURLs.push(`${S3_BASE_URL}/${content.Key}`);
           });
-          return res(imageURLs);
+          return resolve(imageURLs);
         }
       }
     );
   });
 };
 
-export { getStaticImageURLs, s3 };
+const deleteObject = async (
+  path: string
+): Promise<aws.S3.DeleteObjectOutput> => {
+  const params: aws.S3.DeleteObjectRequest = {
+    Bucket: S3_BUCKET_ID,
+    Key: path,
+  };
+
+  return new Promise((resolve, reject) => {
+    s3.deleteObject(params, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+};
+
+const uploadObject = async (
+  key: string,
+  buffer: ByteBuffer,
+  contentEncoding: string,
+  contentType: string
+): Promise<aws.S3.ManagedUpload.SendData> => {
+  const params: aws.S3.PutObjectRequest = {
+    Bucket: S3_BUCKET_ID,
+    Key: key,
+    Body: buffer,
+    ContentEncoding: contentEncoding,
+    ContentType: contentType,
+  };
+  return new Promise((resolve, reject) => {
+    s3.upload(params, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+};
+
+export { getMediaURLS, deleteObject, uploadObject, s3 };
 export default s3;
