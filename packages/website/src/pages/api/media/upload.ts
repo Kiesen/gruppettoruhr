e3301c8607c3bbed835cbeb2fd0bc2991957fb04
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-25 01:29:08 +0100

    Update the media api to use busboy instead of multer.

--- packages/website/src/pages/api/media/upload.ts
+++ packages/website/src/pages/api/media/upload.ts
@@ -1,29 +1,12 @@
-/**
- * -- TODO --
- * Remove eslint disable rule after adding req and res types
- * that work both with next and multer.
- */
 import { NextApiRequest, NextApiResponse } from 'next';
-import multer from 'multer';
-import multerS3 from 'multer-s3';
-import s3 from '@src/utils/s3';
+import { getSession } from 'next-auth/client';
+import { v4 as uuidv4 } from 'uuid';
+import Busboy from 'busboy';
 
+import { uploadObject } from '@src/utils/s3';
 import { createJSONPayload } from '@src/utils/api';
-import {
-  FIELD_NAME,
-  MIME_TYPES,
-  MAX_FILES,
-  MAX_FILE_SIZE,
-} from '@src/consts/upload';
-import {
-  BUCKET_MEDIA_PREFIX,
-  S3_BUCKET_ID,
-  S3_BASE_URL,
-} from '@src/consts/s3';
-
-type NextApiRequestWithMediaProp = NextApiRequest & {
-  mediaURLS?: string[];
-};
+import { MIME_TYPES } from '@src/consts/upload';
+import { BUCKET_MEDIA_PREFIX } from '@src/consts/s3';
 
 export const config = {
   api: {
@@ -31,68 +14,71 @@ export const config = {
   },
 };
 
-const uploadMiddleware = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: S3_BUCKET_ID,
-    acl: 'public-read',
-    contentType(_, file, cb) {
-      cb(null, file.mimetype);
-    },
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    key(req: any, file, cb) {
-      const contentKey = `${BUCKET_MEDIA_PREFIX}/${file.originalname}`;
-
-      // Use the media array on the request object to add new media url
-      req.mediaURLS = [
-        ...req.mediaURLS,
-        `${S3_BASE_URL}/${contentKey}`,
-      ];
-
-      cb(null, contentKey);
-    },
-  }),
-  fileFilter: (_, file, cb) => {
-    if (MIME_TYPES.includes(file.mimetype)) {
-      cb(null, true);
-    }
-    cb(null, false);
-  },
-  limits: {
-    fileSize: MAX_FILE_SIZE,
-    files: MAX_FILES,
-  },
-}).array(FIELD_NAME);
-
-const handler = (
-  req: NextApiRequestWithMediaProp,
+const mediaHandler = async (
+  req: NextApiRequest,
   res: NextApiResponse
-): void => {
-  if (req.method === 'POST') {
-    req.mediaURLS = [];
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    uploadMiddleware(req as any, res as any, (err) => {
-      if (err) {
-        const payload = createJSONPayload<string>(req.method, {
-          error: 'Internal error during image upload',
-        });
-        res.status(500).send(payload);
-      }
+): Promise<void> => {
+  /**
+   * We need to return a promise to avoid that nextjs report a warning.
+   * The reason for this is the nested async function call on the
+   * busboy on file event.
+   */
+  return new Promise((resolve) => {
+    if (req.method === 'POST') {
+      const session = getSession({ req });
+      if (!session) {
+        res.status(401).send({});
+        resolve();
+      } else {
+        const busboy = new Busboy({ headers: req.headers });
+        const rejectedFiles: string[] = [];
+        const mediaURLS: string[] = [];
+        let httpStatus = 200;
+        let counter = 0;
 
-      const payload = createJSONPayload<{ mediaURLS: string[] }>(
-        req.method,
-        {
-          data: { mediaURLS: req.mediaURLS },
-        }
-      );
-      res.status(200).send(payload);
-    });
-  } else {
-    const payload = createJSONPayload<string>(req.method, {
-      error: 'Method not allows',
-    });
-    res.status(405).send(payload);
-  }
+        busboy.on(
+          'file',
+          async (_, file, filename, encoding, mimetype) => {
+            counter = counter + 1;
+            if (!MIME_TYPES.includes(mimetype)) {
+              httpStatus = 415;
+              rejectedFiles.push(filename);
+            } else {
+              const uuid = uuidv4();
+
+              const uploadData = await uploadObject(
+                `${BUCKET_MEDIA_PREFIX}/${uuid}.${filename}`,
+                file,
+                encoding,
+                mimetype
+              );
+              mediaURLS.push(uploadData.Location);
+              counter = counter - 1;
+              console.log(counter, mediaURLS);
+              if (counter === 0) {
+                if (httpStatus == 415) {
+                  res.status(415);
+                  res.send({ mediaURLS, rejectedFiles });
+                  resolve();
+                } else {
+                  res.status(200);
+                  res.send({ mediaURLS });
+                  resolve();
+                }
+              }
+            }
+          }
+        );
+        req.pipe(busboy);
+      }
+    } else {
+      const payload = createJSONPayload<string>(req.method, {
+        error: 'Method not allows',
+      });
+      res.status(405).send(payload);
+      resolve();
+    }
+  });
 };
 
-export default handler;
+export default mediaHandler;
