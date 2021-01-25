05e8ef453c49b6222ce4fc8f32e604e37150bb4a
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-25 01:50:22 +0100

    Add from query string to login route.

--- packages/website/src/components/Unauthorized.tsx
+++ packages/website/src/components/Unauthorized.tsx
@@ -8,36 +8,33 @@ import {
 
 type UnauthorizedProps = {
   classes?: string;
-  backLink?: { href: string; name: string };
 };
 
-const Unauthorized: FC<UnauthorizedProps> = ({
-  classes,
-  backLink,
-}) => (
-  <div
-    className={` h-full bg-gray-100 flex flex-col justify-center items-center ${classes}`}
-  >
-    <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-      <h1 className="text-2xl text-center font-bold mb-8">
-        You are not authorized to see this content
-      </h1>
-      <span className="w-full">
-        <Link href="/login">
-          <a className={`mb-4 ${primaryActionButtonClasses}`}>
-            Login
-          </a>
-        </Link>
-        {backLink && (
-          <Link href={backLink.href}>
-            <a className={`mb-4 ${secondaryActionButtonClasses}`}>
-              {backLink.name}
+const Unauthorized: FC<UnauthorizedProps> = ({ classes }) => {
+  const fromURLEncode = encodeURIComponent(window.location.pathname);
+  return (
+    <div
+      className={` h-full bg-gray-100 flex flex-col justify-center items-center ${classes}`}
+    >
+      <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+        <h1 className="text-2xl text-center font-bold mb-8">
+          You are not authorized to see this content
+        </h1>
+        <span className="w-full">
+          <Link href={`/login?from=${fromURLEncode}`}>
+            <a className={`mb-4 ${primaryActionButtonClasses}`}>
+              Login
             </a>
           </Link>
-        )}
-      </span>
+          <Link href="/">
+            <a className={`mb-4 ${secondaryActionButtonClasses}`}>
+              Home
+            </a>
+          </Link>
+        </span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Unauthorized;
