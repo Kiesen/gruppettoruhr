ba34215e4859cc39f5fa7e0a95e4dbe3590df2b3
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-27 00:24:05 +0100

    Update the upload limits.

--- packages/website/src/consts/upload.ts
+++ packages/website/src/consts/upload.ts
@@ -1,4 +1,4 @@
 export const FIELD_NAME = 'media';
-export const MAX_FILES = 5;
-export const MAX_FILE_SIZE = 2e6;
+export const MAX_FILES = 1;
+export const MAX_FILE_SIZE = 1024 * 1024 * 20;
 export const MIME_TYPES = ['image/jpeg', 'image/png', 'image/gif'];
