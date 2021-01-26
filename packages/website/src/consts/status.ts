70c2e1ac5b1a978c724bbf2eef50a9ca444712bc
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-27 00:23:43 +0100

    Add http status codes as consts.

--- packages/website/src/consts/status.ts
+++ packages/website/src/consts/status.ts
@@ -1,5 +1,7 @@
 export const HTTP_OK = 200;
-export const HTTP_NOT_FOUND = 400;
+export const HTTP_BAD_REQUEST = 400;
+export const HTTP_NOT_FOUND = 404;
 export const HTTP_UNAUTHORIZED = 401;
 export const HTTP_METHOD_NOT_ALLOWED = 405;
 export const HTTP_UNSUPPORTED_MEDIA_TYPE = 415;
+export const HTTP_INTERNAL_SERVER_ERROR = 500;
