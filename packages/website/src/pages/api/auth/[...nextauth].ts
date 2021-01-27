6435a904bc92b375d1debe7c4c41017a27619f26
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-27 01:09:12 +0100

    Update the credentials login.

--- packages/website/src/pages/api/auth/[...nextauth].ts
+++ packages/website/src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth, { InitOptions } from 'next-auth';
 import Providers from 'next-auth/providers';
+import bcrypt from 'bcrypt';
 import querystring from 'querystring';
 
 const options: InitOptions = {
@@ -24,29 +25,33 @@ const options: InitOptions = {
        */
       credentials: {},
       authorize: async (credentials) => {
-        let user = null;
         /**
          * -- TODO --
-         * This is just a temporary solution during development
-         * and should be removed in production!
+         * Changes this logic once we decided how we want to handle member
+         * data and if we even need to. For now the functionality is secure
+         * enough as we don't store and need to hide any sensitive data.
          */
-        if (
-          credentials['password'] === process.env.TMP_PWD &&
-          credentials['username'] === process.env.TMP_USER
-        ) {
-          user = {
+        const passwordMatched = await bcrypt.compare(
+          credentials['password'],
+          process.env.TMP_PASSWORD
+        );
+        const usernameMatches =
+          credentials['username'] === process.env.TMP_USER;
+        if (passwordMatched && usernameMatches) {
+          return Promise.resolve({
             id: 1,
             name: 'Gruppettoruhr',
-          };
-        }
-        if (user) {
-          return Promise.resolve(user);
-        } else {
-          const path = querystring.stringify({
-            error: true,
           });
-          return Promise.reject(`/login?${path}`);
         }
+        const path = querystring.stringify({
+          error: true,
+        });
+        /**
+         * -- TODO --
+         * Also add the previous location path if there is one
+         * to the query string
+         */
+        return Promise.reject(`/login?${path}`);
       },
     }),
   ],
