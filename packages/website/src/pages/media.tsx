cd8ebf24ad50ff1e61554e6e2d03c52ab2402aa7
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-01-29 01:06:18 +0100

    Update the logic and apperance of the media page.

--- packages/website/src/pages/media.tsx
+++ packages/website/src/pages/media.tsx
@@ -1,116 +1,116 @@
 import Head from 'next/head';
 import { GetServerSidePropsContext } from 'next';
-import { FC, SyntheticEvent, useState } from 'react';
+import { FC, useState, SyntheticEvent } from 'react';
 import Dropzone, { DropzoneOptions } from 'react-dropzone';
 import { useSession, getSession, Session } from 'next-auth/client';
-import { BiWindowOpen, BiCloudUpload, BiCopy } from 'react-icons/bi';
+import {
+  BiCloudUpload,
+  BiLinkExternal,
+  BiLinkAlt,
+  BiTrash,
+} from 'react-icons/bi';
 import { AiOutlineLoading } from 'react-icons/ai';
 
+import {
+  useMediaQuery,
+  useUploadMediaContentMutation,
+  useDeleteMediaContentMutation,
+} from '@src/hooks/media';
 import Unauthorized from '@src/components/Unauthorized';
-import { getContentURLS } from '@src/utils/s3';
-import { dropzoneClasses } from '@src/styles/media';
-import { primaryActionButtonClasses } from '@src/styles/buttons';
 import {
   showErrorNotification,
   showSuccessNotification,
 } from '@src/components/Notifications';
-import { BUCKET_MEDIA_PREFIX } from '@src/consts/s3';
+import { dropzoneClasses } from '@src/styles/media';
+import { secondaryActionButtonClasses } from '@src/styles/buttons';
 import {
   FIELD_NAME,
   MIME_TYPES,
-  MAX_FILES,
   MAX_FILE_SIZE,
 } from '@src/consts/upload';
 
 type MediaProps = {
-  urls: string[];
   session?: Session;
 };
 
-type MediaState = {
-  urls: string[];
-  uploads: string[];
-};
+type MediaState = string[];
 
-/**
- * -- TODO --
- * Check if getContentURLS can be moved or replaced
- * once react query is in place.
- */
 export const getServerSideProps = async (
   context: GetServerSidePropsContext
 ): Promise<{
   props: MediaProps;
 }> => {
   let session = undefined;
-  let urls = [];
   try {
     session = await getSession(context);
-    urls = await getContentURLS(BUCKET_MEDIA_PREFIX);
   } catch (error) {
     console.error(error);
   }
   return {
-    props: { urls, session },
+    props: { session },
   };
 };
 
-const Media: FC<MediaProps> = ({ urls }) => {
-  const [state, setState] = useState<MediaState>({
-    urls,
-    uploads: [],
-  });
-  const [session, loading] = useSession();
+const dropzoneOptions: Partial<DropzoneOptions> = {
+  maxSize: MAX_FILE_SIZE,
+  accept: MIME_TYPES,
+};
 
-  const dropzoneOptions: Partial<DropzoneOptions> = {
-    maxFiles: MAX_FILES,
-    maxSize: MAX_FILE_SIZE,
-    accept: MIME_TYPES,
-  };
+const Media: FC<MediaProps> = () => {
+  const [uploads, setUploads] = useState<MediaState>([]);
+  const [session, sessionLoading] = useSession();
+  const { data: mediaItems, isLoading } = useMediaQuery();
+  const {
+    mutateAsync: mutateAsyncUpload,
+  } = useUploadMediaContentMutation();
+  const {
+    mutateAsync: mutateAsyncDelete,
+  } = useDeleteMediaContentMutation();
 
   /**
    * -- TODO --
-   * Consider uploading the images not as batch
-   * but async one by one.
+   * Update the setting of the local state. Right now it is not
+   * working because of lost updates.
    */
   const onDropImages = (acceptedFiles: File[]) => {
-    const formData = new FormData();
-    let uploads: string[] = [];
     acceptedFiles.forEach((file) => {
+      setUploads([...uploads, file.name]);
+      const formData = new FormData();
       formData.append(FIELD_NAME, file);
-      uploads = [...uploads, file.name];
-    });
 
-    setState({ ...state, uploads });
-
-    /**
-     *  -- TODO --
-     * Replace the fetch with react query and
-     * add api response types.
-     */
-    fetch(`api/media/upload`, {
-      method: 'POST',
-      body: formData,
-    })
-      .then((res) => res.json())
-      .then(({ acceptedFiles, rejectedFiles }) => {
-        for (const file in rejectedFiles as string[]) {
+      mutateAsyncUpload(formData)
+        .then(() => {
+          showSuccessNotification(`Upload successful: ${file.name}`);
+        })
+        .catch((error) => {
+          console.error(error);
           showErrorNotification(
-            `Error during upload of file: ${file}`
+            `Error during uploading: ${file.name}`
           );
-        }
-        const urls = acceptedFiles.concat(state.urls);
-        setState({ urls, uploads: [] });
-      })
-      .catch((error) => {
-        console.error(error);
-        showErrorNotification('Error during image upload');
-      });
+        })
+        .finally(() => {
+          setUploads(
+            uploads.filter((filename) => filename !== file.name)
+          );
+        });
+    });
   };
 
-  const copyToClipboard = (
+  const deleteMediaItem = async (
     event: SyntheticEvent<HTMLButtonElement>
-  ) => {
+  ): Promise<void> => {
+    event.preventDefault();
+    const id = event.currentTarget.id;
+    try {
+      await mutateAsyncDelete(id);
+      showSuccessNotification(`File successful deleted`);
+    } catch (error) {
+      console.error(error);
+      showErrorNotification(`Could not remove item`);
+    }
+  };
+
+  const copyURL = (event: SyntheticEvent<HTMLButtonElement>) => {
     event.preventDefault();
     const textField = document.createElement('textarea');
     textField.innerText = event.currentTarget.id;
@@ -118,23 +118,24 @@ const Media: FC<MediaProps> = ({ urls }) => {
     textField.select();
     document.execCommand('copy');
     textField.remove();
-    showSuccessNotification('URL copied to clipboard');
+    showSuccessNotification('Link copied to clipboard');
   };
 
   /**
    * -- TODO --
-   * Add an option to delete an image from the s3 space
+   * Update the appearance of the action buttons e.g. add hover
+   * with a transition and try out different colors.
    */
-  // const deleteImage = () => ({});
-
   return (
     <>
       <Head>
         <title>Gruppettoruhr - Media</title>
       </Head>
       <main className="min-h-screen bg-gray-100">
-        {!loading && !session && <Unauthorized classes="h-screen" />}
-        {!loading && session && (
+        {!sessionLoading && !session && (
+          <Unauthorized classes="h-screen" />
+        )}
+        {!sessionLoading && session && (
           <div className="container mx-auto py-24">
             <Dropzone onDrop={onDropImages} {...dropzoneOptions}>
               {({ getRootProps, getInputProps }) => (
@@ -151,42 +152,54 @@ const Media: FC<MediaProps> = ({ urls }) => {
               )}
             </Dropzone>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 md:gap-8">
-              {state.uploads.map((id) => (
+              {uploads.map((filename) => (
                 <div
-                  key={id}
+                  key={filename}
                   className="bg-white border p-4 rounded-xl flex flex-col items-center justify-center"
                 >
                   <AiOutlineLoading className="animate-spin h-12 w-12 mb-5" />
-                  Uploading...
+                  {`Uploading ${filename}...`}
                 </div>
               ))}
-              {state.urls.map((url) => (
-                <div
-                  key={url}
-                  className=" bg-white border p-4 rounded-xl flex flex-col items-center justify-between"
-                >
-                  <img src={url} className="max-w-full max-h-36" />
-                  <div className="w-full mt-8">
-                    <button
-                      id={url}
-                      className={`mb-4 ${primaryActionButtonClasses}`}
-                      onClick={copyToClipboard}
-                    >
-                      <BiCopy className="h-5 w-5 mr-2 hidden sm:inline-block" />
-                      Copy Link
-                    </button>
-                    <a
-                      href={url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className={`${primaryActionButtonClasses}`}
-                    >
-                      <BiWindowOpen className="h-5 w-5 mr-2 hidden sm:inline-block" />{' '}
-                      Open Image
-                    </a>
+              {!isLoading &&
+                mediaItems.map((mediaItem) => (
+                  <div
+                    key={mediaItem.key}
+                    className=" bg-white border p-4 rounded-xl flex flex-col items-center justify-between"
+                  >
+                    <img
+                      src={mediaItem.url}
+                      className="max-w-full max-h-36"
+                    />
+                    <div className="w-full mt-8">
+                      <button
+                        id={mediaItem.url}
+                        className={`mb-4 ${secondaryActionButtonClasses}`}
+                        onClick={copyURL}
+                      >
+                        <BiLinkAlt className="h-5 w-5 mr-2 inline-block" />
+                        Copy Link
+                      </button>
+                      <div className="flex flex-row justify-evenly">
+                        <a
+                          href={mediaItem.url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className={`mr-2 ${secondaryActionButtonClasses}`}
+                        >
+                          <BiLinkExternal className="h-5 w-5 mr-2 inline-block" />{' '}
+                        </a>
+                        <button
+                          id={mediaItem.key}
+                          className={`ml-2 bg-red-500 border-red-500 text-white ${secondaryActionButtonClasses}`}
+                          onClick={deleteMediaItem}
+                        >
+                          <BiTrash className="h-5 w-5 mr-2 inline-block" />{' '}
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
             </div>
           </div>
         )}
