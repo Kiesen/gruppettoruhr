e9dd0660bad078fbd371b4eeed14529c1a179348
Frederik Maximilian Aulich <frederik.aulich@uni-duesseldorf.de>
2021-02-18 00:24:28 +0100

    Add section styles.

---
+++ packages/website/src/styles/section.ts
@@ -0,0 +1,21 @@
+export const sizeWindow = `
+  w-screen 
+  h-screen 
+`
+  .replace(/\s+/g, ' ')
+  .trim();
+
+export const sizeContent = `
+  py-12 
+  w-full
+`
+  .replace(/\s+/g, ' ')
+  .trim();
+
+export const backgroundImage = `
+  bg-no-repeat 
+  bg-cover 
+  bg-fixed bg-center
+`
+  .replace(/\s+/g, ' ')
+  .trim();
